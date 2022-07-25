f20362e4c9fde17de51ffbcfcf6cb39867effb5b
debora958 <109834177+debora958@users.noreply.github.com>
2022-07-25 19:21:07 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+
+def calculadora(n1, n2, operação):
+    if (operação == 1):
+        resposta = n1 + n2
+    elif (operação == 2):
+        resposta = n1 - n2
+    elif (operação == 3):
+        resposta = n1 * n2
+    elif (operação == 4):
+        resposta = n1 / n2
+    else :
+        resposta = 0
+    print(resposta)
+
+calculadora (7,2,3)
+    
